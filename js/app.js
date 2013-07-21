5f3f3b311e0d8886295d461737e374ae1a6ffa9b
amadeus junqueira <amadeusjunqueira@gmail.com>
2013-07-21 15:23:48 -0400

    removing superfluous cruft, keeping other cruft he he

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
 define(function(require){
-  //var $ = require('jquery');
-  //var _ = require('underscore');
-  //var Backbone = require('backbone');
   var Marionette = require('marionette');
-  console.log();
 });
